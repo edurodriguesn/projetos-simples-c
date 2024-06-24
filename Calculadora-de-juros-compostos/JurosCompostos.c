a9597585092b3617ce114e10a955d42048491f33
Eduardo Rodrigues <edu.rodrigues.ap@gmail.com>
2024-06-24 00:11:44 -0300

    .

---
+++ Calculadora-de-juros-compostos/JurosCompostos.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include <math.h>
+#include <string.h>
+int main()
+{
+    float valor,taxa,quantidade_do_periodo,total;
+    int tipotaxa,tipo_de_periodo;
+    char identificao_de_periodo[6];
+
+    printf("Valor R$: ");
+    scanf("%f",&valor);
+    printf("Taxa(%%): ");
+    scanf("%f",&taxa);
+    printf("1- Ao dia\n2- Ao mês\n3- Ao ano\n");
+    do
+        scanf("%d",&tipotaxa);
+    while(tipotaxa<1||tipotaxa>3);
+
+    printf("Seu período será em:\n1- Dias\n2- Meses\n3- Anos\n");
+    do
+        scanf("%d",&tipo_de_periodo);
+    while(tipo_de_periodo<1||tipo_de_periodo>3);
+
+    if(tipo_de_periodo==1)
+        strcpy(identificao_de_periodo,"Dias");
+        else if(tipo_de_periodo==2)
+            strcpy(identificao_de_periodo,"Meses");
+            else
+                strcpy(identificao_de_periodo,"Anos");
+
+    printf("Quantidade de %s: ",identificao_de_periodo);
+    scanf("%f",&quantidade_do_periodo);
+
+    if(tipotaxa==1)
+    {
+        if(tipo_de_periodo==2)
+            quantidade_do_periodo*=30;
+            else if(tipo_de_periodo==3)
+                quantidade_do_periodo*=365;
+    }
+    else if(tipotaxa==2)
+    {
+        if(tipo_de_periodo==1)
+            quantidade_do_periodo/=30;
+            else if(tipo_de_periodo==3)
+                quantidade_do_periodo*=12;
+    }
+    else
+        if(tipo_de_periodo==1)
+            quantidade_do_periodo/=365;
+            else if(tipo_de_periodo==2)
+                quantidade_do_periodo/=12;
+
+    total=valor*pow(1+(taxa/100),quantidade_do_periodo);
+
+    printf("Total R$ %.2f",total);
+    
+    return 0;
+}
---
+++ Calculadora-de-juros-compostos/README.md
@@ -0,0 +1,9 @@
+# Objetivo do programa
+Este programa é uma calculadora de juros compostos e tem a função para os 3 tipos mais comuns de período (ao dia, mês e ano).
+
+## Funções auxiliares
+- Recebe os valores, taxa e tipo de período
+- Aplica a formula de juros compostos para cada tipo de período
+
+## Melhorias a serem feitas
+- 
